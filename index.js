0e053f41b978150f9b13595d2dfa6cdf02e44155
Mihail Curmei <mcurmei@mail.ru>
2022-12-12 22:55:26 +0200

    standard layout update

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+//TODO get data from DB
+let horses = []
+let bets = []
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+  
+  // about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.post('/bets', (req, res) => {
+    //show own bets by default and all bets with admin rights
+});
+
+app.post('/horses', (req, res) => {
+    //TODO verify authentication
+    // We will be coding here
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
